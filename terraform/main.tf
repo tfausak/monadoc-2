de935be4d03dfcacea5812500bbc3de357e2472f
Taylor Fausak <taylor@fausak.me>
2019-10-21 21:06:09 -0400

    Remove "publish" Terraform variable

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,7 +5,7 @@ terraform {
   # https://www.terraform.io/docs/backends/types/s3.html
   backend "s3" {
     bucket = "monadoc"
-    key = "monadoc.tfstate"
+    key    = "monadoc.tfstate"
     region = "us-east-1"
   }
 }
@@ -19,18 +19,15 @@ variable "commit" {
   type = string
 }
 
-variable "publish" {
-  default = false
-  type = bool
-}
-
 # https://www.terraform.io/docs/providers/aws/r/lambda_function.html
 resource "aws_lambda_function" "this" {
   function_name = "arn:aws:lambda:us-east-1:014479108335:function:example-haskell-lambda-function"
-  handler = "hello.handler"
-  publish = var.publish
-  role = "arn:aws:iam::014479108335:role/service-role/example-haskell-lambda-function-role-vrvm2inv"
-  runtime = "provided"
-  s3_bucket = "monadoc"
-  s3_key = "functions/${var.commit}.zip"
+  handler       = "hello.handler"
+  publish       = true
+  role          = "arn:aws:iam::014479108335:role/service-role/example-haskell-lambda-function-role-vrvm2inv"
+  runtime       = "provided"
+  s3_bucket     = "monadoc"
+  s3_key        = "functions/${var.commit}.zip"
 }
+
+# TODO: Import more resources into Terraform.
